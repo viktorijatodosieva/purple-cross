11db8f48ba84cc5d788cd6204e875a47d2ac60a2
Viktorija Todosieva <viktorijatodosieva7@gmail.com>
2026-04-29 16:42:27 +0200

    Set up Vue Router and project structure

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import {createRouter, createWebHistory} from 'vue-router'
+
+const router = createRouter({history: createWebHistory(), routes: [
+    {path: '/', redirect: '/employees'},
+    {path: '/employees', component: () => import('../views/EmployeeList.vue')},
+    {path: '/employees/create', component: () => import('../views/EmployeeForm.vue')},
+    {path: '/employees/:id', component: () => import('../views/EmployeeProfile.vue')},
+    {path: '/employees/:id/edit', component: () => import('../views/EmployeeForm.vue')}
+    ]})
+
+export default router
--- src/views/EmployeeList.vue
+++ src/views/EmployeeList.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <RouterView />
+<h1>Employee List</h1>
 </template>
 
 <style scoped>
